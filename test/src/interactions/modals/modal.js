8bf54e2cb4dba77db6e9d4f596fa82ca6c1e2393
skyro83 <100965491+skyro83@users.noreply.github.com>
2024-04-07 14:23:25 +0000

    Init

---
+++ test/src/interactions/modals/modal.js
@@ -0,0 +1,11 @@
+const { Modal } = require("sheweny");
+
+module.exports = class ModalComponent extends Modal {
+  constructor(client) {
+    super(client, ["modal-id"]);
+  }
+  
+  async execute(modal) {
+    modal.reply("Modal received.");
+  }
+};
